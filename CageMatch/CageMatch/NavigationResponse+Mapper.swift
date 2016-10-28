d1fa3980ca1a6951ea5fcdafaf731eb8cc77e2e0
Kevin Taniguchi <taniguchik1@uoi-it-taniguchik1.local>
2016-10-28 09:10:55 -0500

    removed commented code

--- CageMatch/CageMatch/NavigationResponse+Mapper.swift
+++ CageMatch/CageMatch/NavigationResponse+Mapper.swift
@@ -55,33 +55,3 @@ class NavigationResponseMapper: NSObject, Mappable {
         mainNavigationItems = parsedNavItems
     }
 }
-
-//class NavigationResponseMarshal: NSObject, Unmarshaling {
-//    let navigationItems: [NavigationItemMarshal]
-//    
-//    required init(object: MarshaledObject) throws {
-//        var parsedNavItems = [NavigationItemMarshal]()
-//        if let rawNavItems: [NSDictionary] = try object.value(for: "navigationItems"), let rawMainItem = rawNavItems.first, let rawMainChildren: [[String: Any]] = try? rawMainItem.value(for: "navigationItems") {
-//            for rawChild in rawMainChildren {
-//                if let ni: NavigationItemMarshal = try? rawChild.value(for: "") {
-//                    parsedNavItems.append(ni)
-//                }
-//                
-//                
-//                print("rawChild name = \(rawChild["slug"])")
-//            }
-//        }
-//        
-//        // This throws out the entire damn thing because there is no optional array parsing
-//        //        let navItems: [NavigationItemMarshal]? = try? object.value(for: "navigationItems")
-//        //        guard let mainNavChildren = navItems?.first?.children else {
-//        //            throw NavigationResponseMappingError.missingNavigationItemsError
-//        //        }
-//        
-//        if parsedNavItems.count == 0 {
-//            throw NavigationResponseMappingError.missingNavigationItemsError
-//        }
-//        
-//        navigationItems = parsedNavItems
-//    }
-//}
