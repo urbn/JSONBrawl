a87a6b96e7ca5668e330bd84eacfdcf89a3cba24
Ryan Garchinsky <ryang1428@gmail.com>
2016-10-28 08:57:08 -0400

    Trying out PMJSON

---
+++ CageMatch/CageMatch/NavigationItem+PMJSON.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationItem+PMJSON.swift
+//  CageMatch
+//
+//  Created by Ryan Garchinsky on 10/28/16.
+//  Copyright © 2016 URBN. All rights reserved.
+//
+
+import Foundation
---
+++ CageMatch/CageMatch/NavigationResponse+PMJSON.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationResponse+PMJSON.swift
+//  CageMatch
+//
+//  Created by Ryan Garchinsky on 10/28/16.
+//  Copyright © 2016 URBN. All rights reserved.
+//
+
+import Foundation
